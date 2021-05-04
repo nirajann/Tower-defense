250780b656fbe38dc4aa552a8863dd23fe7f5b84
Nirajan <gnirajan46@gmail.com>
2021-05-04 20:08:56 +0545

    initial commit

--- tower-defense/game.py
+++ tower-defense/game.py
@@ -1,39 +1,112 @@
 import pygame
+import os
 
 pygame.init()
 
-height = 600
-width = 600
-win = pygame.display.set_mode((height,width))
+win_height = 1000
+win_width = 500
+win = pygame.display.set_mode((win_height,win_width))
 pygame.display.set_caption("Gate keepers")
+bg = pygame.transform.scale(pygame.image.load(os.path.join("Assets","Background.png")),(win_height,win_width))
 
-x = 300
-y = 300
-radius = 15
-vel = 10
+# Load Images of the Character (there are two popular ways)
+stationary = pygame.image.load(os.path.join("Assets/Hero", "standing.png"))
+# One way to do it - using the sprites that face left.
+
+left =  [pygame.image.load(os.path.join("Assets/Hero", "L1.png")),
+         pygame.image.load(os.path.join("Assets/Hero", "L2.png")),
+         pygame.image.load(os.path.join("Assets/Hero", "L3.png")),
+         pygame.image.load(os.path.join("Assets/Hero", "L4.png")),
+         pygame.image.load(os.path.join("Assets/Hero", "L5.png")),
+         pygame.image.load(os.path.join("Assets/Hero", "L6.png")),
+         pygame.image.load(os.path.join("Assets/Hero", "L7.png")),
+         pygame.image.load(os.path.join("Assets/Hero", "L8.png")),
+         pygame.image.load(os.path.join("Assets/Hero", "L9.png"))]
+
+right = [pygame.image.load(os.path.join("Assets/Hero", "R1.png")),
+         pygame.image.load(os.path.join("Assets/Hero", "R2.png")),
+         pygame.image.load(os.path.join("Assets/Hero", "R3.png")),
+         pygame.image.load(os.path.join("Assets/Hero", "R4.png")),
+         pygame.image.load(os.path.join("Assets/Hero", "R5.png")),
+         pygame.image.load(os.path.join("Assets/Hero", "R6.png")),
+         pygame.image.load(os.path.join("Assets/Hero", "R7.png")),
+         pygame.image.load(os.path.join("Assets/Hero", "R8.png")),
+         pygame.image.load(os.path.join("Assets/Hero", "R9.png"))]
+
+
+
+class hero:
+    def __init__(self,x,y):
+        #walk
+        self.x = x
+        self.y = y
+        self.velx =10
+        self.vely =10
+        self.face_right = True
+        self.face_left = False
+        self.stepIndex = 0
+        #jump
+        self.jump = False
+
+    def move_hero(self,userInput):
+        if userInput[pygame.K_RIGHT] and self.x <= 950:
+            self.x += self.velx
+            self.face_right = True
+            self.face_left = False
+        elif userInput[pygame.K_LEFT] and self.x >= 0:
+            self.x -= self.velx
+            self.face_right = False
+            self.face_left = True
+        else:
+            self.stepIndex = 0
+
+    def draw(self,win):
+        if self.stepIndex >= 9:
+            self.stepIndex = 0
+        elif self.face_left:
+            win.blit(left[self.stepIndex],(self.x,self.y))
+            self.stepIndex += 1
+        if self.face_right:
+            win.blit(right[self.stepIndex],(self.x,self.y))
+            self.stepIndex += 1
+    def jump_motion(self,userInput):
+        if userInput[pygame.K_SPACE] and self.jump is False:
+            self.jump = True
+        if self.jump is True:
+            self.y -= self.vely * 2
+            self.vely -= 1
+        if self.vely < -10:
+            self.jump = False
+            self.vely = 10
+
+
+def draw_game():
+    win.fill((0,0,0))
+    win.blit(bg, (0, 0))
+    player.draw(win)
+    pygame.time.delay(30)
+    pygame.display.update()
+
+
+player = hero(250,370)
+
+
+#mainloop
 run = True
 
 while run:
-
-    win.fill((0,0,0))
-    pygame.draw.circle(win,(255, 255, 255),(int(x),int(y)),radius)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
-    #this code is used for movement and keeping the object inside boundires
+    #input
     userInput = pygame.key.get_pressed()
-    if userInput[pygame.K_LEFT] and x > 0:
-        x -= vel
-    if userInput[pygame.K_RIGHT] and x  < 600:
-        x += vel
-    if userInput[pygame.K_UP] and y > 0:
-        y -= vel
-    if userInput[pygame.K_DOWN] and  y < 600:
-        y += vel
 
-    pygame.time.delay(10)
+    #movement
+    player.move_hero(userInput)
+    player.jump_motion(userInput)
 
+    #draw game in window
+    draw_game()
 
-    pygame.display.update()
