aba1d963caeb137b68723ff2aa542eff8c91875f
Nirajan <gnirajan46@gmail.com>
2021-05-06 20:39:46 +0545

    initial commit

--- tower-defense/game.py
+++ tower-defense/game.py
@@ -5,9 +5,11 @@ pygame.init()
 
 win_height = 1000
 win_width = 500
-win = pygame.display.set_mode((win_height,win_width))
+win = pygame.display.set_mode((win_height, win_width))
 pygame.display.set_caption("Gate keepers")
-bg = pygame.transform.scale(pygame.image.load(os.path.join("Assets","Background.png")),(win_height,win_width))
+bg = pygame.transform.scale(pygame.image.load(os.path.join("Assets", "Background.png")), (win_height,win_width))
+bullet_img = pygame.transform.scale(pygame.image.load(os.path.join("Assets/Bullets", "buller.png")), (10, 10))
+
 
 # Load Images of the Character (there are two popular ways)
 stationary = pygame.image.load(os.path.join("Assets/Hero", "standing.png"))
@@ -47,6 +49,9 @@ class hero:
         self.stepIndex = 0
         #jump
         self.jump = False
+        #shoot
+        self.bullets = []
+        self.cool_down_count = 0
 
     def move_hero(self,userInput):
         if userInput[pygame.K_RIGHT] and self.x <= 950:
@@ -69,6 +74,7 @@ class hero:
         if self.face_right:
             win.blit(right[self.stepIndex],(self.x,self.y))
             self.stepIndex += 1
+
     def jump_motion(self,userInput):
         if userInput[pygame.K_SPACE] and self.jump is False:
             self.jump = True
@@ -79,11 +85,56 @@ class hero:
             self.jump = False
             self.vely = 10
 
+    def direction(self):
+        if self.face_right:
+            return 1
+        if self.face_left:
+            return -1
+
+    def cooldown(self):
+        if self.cool_down_count >= 10:
+            self.cool_down_count = 0
+        elif self.cool_down_count > 0:
+            self.cool_down_count += 1
+
+    def shoot(self):
+        self.cooldown()
+        if userInput[pygame.K_e] and self.cool_down_count == 0:
+            bullet = Bullet(self.x,self.y, self.direction())
+            self.bullets.append(bullet)
+            self.cool_down_count = 1
+        for bullet in self.bullets:
+            bullet.move()
+            if bullet.off_screen():
+                self.bullets.remove(bullet)
+
+
+class Bullet:
+    def __init__(self,x,y,direction):
+        self.x =x + 25
+        self.y =y + 35
+        self.direction = direction
+
+
+    def draw_bullet(self):
+        win.blit(bullet_img,(self.x,self.y))
+
+    def move(self):
+       if self.direction == 1:
+           self.x += 15
+       if self.direction == -1:
+           self.x -= 15
+
+    def off_screen(self):
+        return not(self.x >= 0 and self.x <= 800)
+
 
 def draw_game():
     win.fill((0,0,0))
     win.blit(bg, (0, 0))
     player.draw(win)
+    for bullet in player.bullets:
+        bullet.draw_bullet()
     pygame.time.delay(30)
     pygame.display.update()
 
@@ -102,6 +153,9 @@ while run:
 
     #input
     userInput = pygame.key.get_pressed()
+
+    #shoot
+    player.shoot()
 
     #movement
     player.move_hero(userInput)
